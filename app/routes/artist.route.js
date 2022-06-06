070bee030103f661556243b8b22a1d7839658355
priyankareddy666 <105285926+priyankareddy666@users.noreply.github.com>
2022-06-06 14:13:46 -0500

    view album based on artistname completed

--- app/routes/artist.route.js
+++ app/routes/artist.route.js
@@ -5,5 +5,7 @@ module.exports = app => {
     router.post("/", artists.create);
     // Retrieve all artists
   router.get("/", artists.findAll);
+    // Retrieve a single Tutorial with id
+    router.get("/:artist", artists.findOne);
     app.use('/api/artists', router);
   };
